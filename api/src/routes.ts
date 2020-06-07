3ee603def24e4259bc3d562bd2b75241460eee32
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-06-07 03:38:20 -0300

    Adicionando validações na rota de cadastro de points

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -4,6 +4,8 @@ import ItemsController from './controllers/ItemsController';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import { celebrate, Joi } from 'celebrate';
+
 const routes = express.Router();
 const upload = multer(multerConfig);
 
@@ -15,6 +17,22 @@ routes.get('/items', itemsController.index);
 routes.get('/points/:id', pointController.show);
 routes.get('/points', pointController.index);
 
-routes.post('/points', upload.single('image'), pointController.create);
+routes.post('/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required()
+        })
+    }, {
+        abortEarly: false
+    }),
+    pointController.create);
 
 export default routes;
